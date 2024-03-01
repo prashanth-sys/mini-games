71b168901c592d362418a23f2f054c5eb79ed18b
root <reddyprashanth512@gmail.com>
2024-03-01 09:37:46 +0000

    solution

--- src/components/MemoryMatrix/index.js
+++ src/components/MemoryMatrix/index.js
@@ -1,15 +1,21 @@
 import {Component} from 'react'
+
 import {FaArrowLeft} from 'react-icons/fa'
+
 import RulesModal from '../RulesModel'
+
 import './index.css'
 
 class MemoryMatrix extends Component {
   state = {
     highlightedIndices: [],
+
+    isModelOpen: false,
   }
 
   componentDidMount() {
     this.getGridButtons()
+
     this.intervalId = setInterval(this.getGridButtons, 6000)
   }
 
@@ -19,14 +25,48 @@ class MemoryMatrix extends Component {
 
   getGridButtons = () => {
     const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     const shuffledArray = myArray.sort(() => Math.random() - 0.5)
+
     const slicedArray = shuffledArray.slice(0, 3)
+
     this.setState({highlightedIndices: slicedArray}, () => {
       setTimeout(() => {
         this.setState({highlightedIndices: []})
       }, 3000)
     })
+
     console.log('New grid buttons:', slicedArray)
+  }
+
+  onClickCell = index => {
+    clearInterval(this.intervalId)
+
+    const {highlightedIndices} = this.state
+
+    if (highlightedIndices.includes(index)) {
+      console.log('matched')
+
+      this.getButtonClass()
+    } else {
+      console.log('not matched')
+
+      this.intervalId = setInterval(this.getGridButtons, 6000)
+    }
+  }
+
+  getButtonClass(index) {
+    const {highlightedIndices} = this.state
+
+    if (highlightedIndices.includes(index + 1)) {
+      return 'highlight'
+    }
+
+    if (highlightedIndices.includes(index)) {
+      return 'matched'
+    }
+
+    return ''
   }
 
   toggleModel = () => {
@@ -35,30 +75,22 @@ class MemoryMatrix extends Component {
     }))
   }
 
-  onClickCell = index => {
-    clearInterval(this.intervalId)
-    const {highlightedIndices} = this.state
-    if (highlightedIndices.includes(index)) {
-      console.log('matched')
-    } else {
-      console.log('not matched')
-      this.intervalId = setInterval(this.getGridButtons, 6000)
-    }
-  }
-
   nextLevel = () => {}
 
   render() {
-    const {highlightedIndices, isModelOpen} = this.state
+    const {isModelOpen} = this.state
 
     return (
       <div className="memory-matrix-container">
         <div className="game-rules-container">
           <button type="button" className="back-button">
             <FaArrowLeft className="icon" />
+
             <p className="back">Back</p>
           </button>
+
           <RulesModal isOpen={isModelOpen} onClose={this.toggleModel} />
+
           <button
             type="button"
             className="rules-button"
@@ -67,19 +99,21 @@ class MemoryMatrix extends Component {
             Rules
           </button>
         </div>
+
         <h1 className="game-heading">Memory Matrix</h1>
+
         <div className="level-container">
           <p className="level">Level-1</p>
+
           <p className="level">Max Level-00</p>
         </div>
+
         <div className="game-container">
           {Array.from({length: 9}, (_, index) => (
             <button
               key={index}
               type="button"
-              className={`button ${
-                highlightedIndices.includes(index + 1) ? 'highlight' : ''
-              }`}
+              className={`button ${this.getButtonClass(index)}`}
               onClick={() => this.onClickCell(index)}
             >
               {_}
